e593d68939dd76de54ac849d23c332a33015f16b
tracer-ysyx <tracer@ysyx.org>
2024-12-11 10:56:36 +0800

    > compile NEMU ysyx_22040000 curryfromuestc Linux curry 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:56:36 up 52 min,  1 user,  load average: 0.22, 0.10, 0.04

---
+++ nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.d
@@ -0,0 +1,31 @@
+cmd_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := unused
+
+source_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := src/engine/interpreter/hostcall.c
+
+deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := \
+  /home/curry/code/ysyx_curry/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+  /home/curry/code/ysyx_curry/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/curry/code/ysyx_curry/nemu/include/macro.h \
+  /home/curry/code/ysyx_curry/nemu/include/debug.h \
+  /home/curry/code/ysyx_curry/nemu/include/cpu/ifetch.h \
+  /home/curry/code/ysyx_curry/nemu/include/memory/vaddr.h \
+  /home/curry/code/ysyx_curry/nemu/include/isa.h \
+  /home/curry/code/ysyx_curry/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/curry/code/ysyx_curry/nemu/include/cpu/difftest.h \
+    $(wildcard include/config/difftest.h) \
+  /home/curry/code/ysyx_curry/nemu/include/difftest-def.h \
+    $(wildcard include/config/isa/x86.h) \
+    $(wildcard include/config/isa/mips32.h) \
+    $(wildcard include/config/isa/riscv.h) \
+    $(wildcard include/config/isa/loongarch32r.h) \
+
+/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o: $(deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o)
+
+$(deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o):
---
+++ nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.d
@@ -0,0 +1,22 @@
+cmd_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o := unused
+
+source_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o := src/isa/riscv32/system/intr.c
+
+deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o := \
+  /home/curry/code/ysyx_curry/nemu/include/isa.h \
+  /home/curry/code/ysyx_curry/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/curry/code/ysyx_curry/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/curry/code/ysyx_curry/nemu/include/macro.h \
+  /home/curry/code/ysyx_curry/nemu/include/debug.h \
+  /home/curry/code/ysyx_curry/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+
+/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o: $(deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o)
+
+$(deps_/home/curry/code/ysyx_curry/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/system/intr.o):
