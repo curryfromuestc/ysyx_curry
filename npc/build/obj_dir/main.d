e2744fa836b51c5131f013a4dfc2e5c9578c481b
curryfromuestc <curryfromuestc@gmail.com>
2024-12-14 14:56:42 +0800

    OSCPU/chisel-playground master initialized
    OSCPU/chisel-playground ecb80339d6655aeed07ae024f0939fcf181dcd32 Merge pull request #10 from unlsycn/master

--- npc/build/obj_dir/main.d
+++
@@ -1,8 +0,0 @@
-main.o: /home/curry/code/ysyx-workbench/npc/csrc/main.cpp \
- /home/curry/code/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/curry/code/ysyx-workbench/nvboard/usr/include/pins.h Vtop.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
