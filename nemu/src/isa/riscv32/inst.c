3195176c1c7560d5d3449f3cb89812dd4106683d
tracer-ysyx <tracer@ysyx.org>
2024-12-16 16:51:09 +0800

    > compile NEMU ysyx_22040000 curryfromuestc Linux curry 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:09 up 11:12,  1 user,  load average: 0.52, 0.24, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,12 +23,12 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_R,
   TYPE_I, 
   TYPE_U, 
   TYPE_S,
   TYPE_J,
   TYPE_B,
+  TYPE_R,
   TYPE_N,//用户中断指令
 };
 
@@ -47,12 +47,12 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
-    case TYPE_R: src1R(); src2R(); break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
