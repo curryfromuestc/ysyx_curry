3e929d9da6e735c1b5d0021b768f0757d920cd04
tracer-ysyx <tracer@ysyx.org>
2025-02-11 19:14:10 +0800

    > compile NEMU ysyx_22040000 curryfromuestc Linux curry 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:14:10 up 2 days, 14:00,  1 user,  load average: 0.02, 0.01, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,106 +5,192 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+
+static void reverse(char *s, int len) {
+  int time=len/2;
+  int cur_time=0;
+  char *end = s + len - 1;
+  char tmp;
+  while (cur_time < time) {
+    tmp = *s;
+    *s = *end;
+    *end = tmp;
+    s++;
+    end--;
+    cur_time++;
+  }
 }
 
-static char *number(char *str, int num, int base)
-{
-  int tmp[100], i = 0;
-  if(num < 0)
-  {
-    *str++ = '-';
-    num = -num;
+/* itoa convert int to string under base. return string length */
+static int itoa(int n, char *s, int base) {
+  assert(base <= 16);
+  if(n!=-2147483648 && n!=0){
+  int i = 0, sign = n, bit;
+  if (sign < 0) n = -n;
+  while (n > 0 ){
+    bit = n % base;
+    if (bit >= 10) s[i++] = 'a' + bit - 10;
+    else s[i++] = '0' + bit;
+    n=n/base;
+    
+  } 
+
+  if (sign < 0) s[i++] = '-';
+  s[i] = '\0';
+
+  reverse(s, i);
+
+  return i;}
+  else if(n==0)
+  {strcpy(s, "0");
+  return 1;}
+  else{strcpy(s, "-2147483648");
+  return 11;}
+}
+
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list pArgs;
+  va_start(pArgs, fmt);
+  char *start = out;
+
+  for (; *fmt != '\0'; ++fmt) {
+    
+    if (*fmt != '%') {
+      *out = *fmt;
+      ++out;
+    } else {
+      switch (*(++fmt)) {
+      case '%': *out = *fmt; ++out; break;
+      case 'd': out += itoa(va_arg(pArgs, int), out, 10); break;
+      case 's':
+        char *s = va_arg(pArgs, char*);
+        strcpy(out, s);
+        out += strlen(out);
+        break;
+      case 'c':
+        char yy=va_arg(pArgs, int);
+        *out = yy;
+        out += 1;
+        break;
+      }
+      
+    }
+    
   }
-  while(num)
+  *out = '\0';
+  va_end(pArgs);
+
+  return out - start;
+}
+
+
+int printf(const char *fmt, ...) {
+  char info[256];
+  for(int i=0;i<256;i++)
   {
-    tmp[i++] = num % base;
-    num /= base;
+    info[i]='\0';
   }
-  
-  i--;
-  
-  while(i >= 0) *str++ = tmp[i--] + '0';   //注意要转化为字符型数字
-  
-  return str;
+  char *buffer = info;
+
+  va_list pArgs;
+  va_start(pArgs, fmt);
+  char *start = buffer;
+  char *start_count = start;
+
+   for (; *fmt != '\0'; ++fmt) {
+    if (*fmt != '%') {
+      *buffer = *fmt;
+      ++buffer;
+    } else {
+      switch (*(++fmt)) {
+      case '%': *buffer = *fmt; ++buffer; break;
+      case 'd': buffer += itoa(va_arg(pArgs, int), buffer, 10); break;
+      case 'x': buffer += itoa(va_arg(pArgs, int), buffer, 16); break;
+      case 'l': switch(*(++fmt)){
+        case 'x':buffer += itoa(va_arg(pArgs,unsigned long int), buffer, 16); break;
+        case 'd':buffer += itoa(va_arg(pArgs,unsigned long int), buffer, 10); break;
+        default : putch('e');putch('\n');putch(*fmt);putch('\n');
+                  panic("\nprintf not all long\n");
+      }break;
+      case '0': ++fmt; int num_0 = *fmt - '0'; ++fmt; int argsn = va_arg(pArgs, int);
+                if((*fmt!='x') && (*fmt!='d')){
+                  putch('e');putch('\n');putch(*fmt);putch('\n');
+                  panic("\nprintf 0x or 0d\n");
+                }
+                else if(*fmt=='x'){
+                  if(itoa(argsn, buffer, 16)<num_0){
+                    for(int i=0;i<num_0-itoa(argsn, buffer, 16);i++){
+                      *buffer = '0';buffer++;
+                    }
+                  }
+                  buffer += itoa(argsn, buffer, 16);
+                }
+                else if(*fmt=='d'){
+                  if(itoa(argsn, buffer, 10)<num_0){
+                    for(int i=0;i<num_0-itoa(argsn, buffer, 10);i++){
+                      *buffer = '0';buffer++;
+                    }
+                  }
+                  buffer += itoa(argsn, buffer, 10);
+                }
+                break;
+      case 's':
+        char *s = va_arg(pArgs, char*);
+        strcpy(buffer, s);
+        buffer += strlen(buffer);
+        break;
+      case 'c':
+        char yy=va_arg(pArgs, int);
+        *buffer = yy;
+        buffer += 1;
+        break;
+      default:
+        if((*fmt-'0')>=0 && (*fmt-'0')<=9){
+          int num_1 = *fmt - '0';fmt++;int argsn = va_arg(pArgs, int);
+          if((*fmt!='x') && (*fmt!='d')){
+            putch('e');putch('\n');putch(*fmt);putch('\n');
+            panic("\nprintf 1x or 1d\n");
+          }
+          else if(*fmt=='x'){
+            if(itoa(argsn, buffer, 16)<num_1){
+              for(int i=0;i<num_1-itoa(argsn, buffer, 16);i++){
+                *buffer = ' ';buffer++;
+              }
+            }
+            buffer += itoa(argsn, buffer, 16);
+          }
+          else if(*fmt=='d'){
+            if(itoa(argsn, buffer, 10)<num_1){
+              for(int i=0;i<num_1-itoa(argsn, buffer, 10);i++){
+                *buffer = ' ';buffer++;
+              }
+            }
+            buffer += itoa(argsn, buffer, 10);
+          }
+        }
+        else{
+          putch('e');putch('\n');putch(*fmt);putch('\n');
+          panic("\nprintf not all geshihua\n");
+        }
+      }
+    }
+  }
+  *buffer = '\0';
+  va_end(pArgs);
+
+    
+    // 逐个字符输出
+    while(*start!='\0'){
+      putch(*start);
+      start++;
+    }
+
+    return buffer-start_count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  //panic("Not implemented");
-  char *str = out;
-  const char *s;
-  int base;   
-  int num;
-
-  for(; *fmt != '\0'; ++fmt)
-  {
-    if(*fmt != '%')
-    {
-      *str++ = *fmt;
-      continue;
-    }
-
-    //*fmt == '%'
-    ++fmt;
-
-    base = 10;  //默认为10进制
-
-    switch(*fmt)
-    {
-      case 'c':
-        *str++ = (unsigned char) va_arg(ap, int);
-        continue;
-      case 's':
-        s = va_arg(ap, char *);
-
-        /****    Plan A    ****/
-
-        //for(int i = 0; s[i]; i++)
-        //  *str++ = s[i];
-
-        /****    Plan B    ****/
-        strcat(str, s);
-        str += strlen(s);
-        continue;
-      case 'd':
-        break;      //用于跳出switch语句
-      default:
-        *str++ = '%';
-        if(*fmt)
-          *str++ = *fmt;
-        else
-          --fmt;    //防止溢出
-        continue;
-    }
-    num = va_arg(ap, int);
-
-    str = number(str, num, base);
-  }
-  *str = '\0';
-  return str - out;  //返回写入的字符数
-}
-
-/*
-stdarg中部分宏的解释
-va_list 是用于存放参数列表的数据结构。
-va_start 函数根据初始化last来初始化参数列表,last为固定参数中的最后一个固定参数.(原因在后面的宏解析中)
-va_arg 函数用于从参数列表中取出一个参数，参数类型由type指定。
-va_copy 函数用于复制参数列表。
-va_end 函数执行清理参数列表的工作。
-*/
-int sprintf(char *out, const char *fmt, ...) {
-  //panic("Not implemented");
-  assert(out);
-
-  va_list args;
-  int i;
-
-  va_start(args, fmt);
-  i = vsprintf(out, fmt, args);
-  va_end(args);
-
-  return i;
+  panic("Not implemented");
 }
 
 
