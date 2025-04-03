1b5190b4d6426eabba43496f0937e5fa68ca3d5b
tracer-ysyx <tracer@ysyx.org>
2025-04-03 13:57:08 +0800

    > compile NEMU ysyx_22040000 curryfromuestc Linux curry 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:57:08 up 4 days,  4:05,  1 user,  load average: 0.05, 0.07, 0.05

---
+++ am-kernels/benchmarks/microbench/build/riscv32-nemu/src/lzip/quicklz.d
@@ -0,0 +1,7 @@
+/home/curry/code/ysyx_curry/am-kernels/benchmarks/microbench/build/riscv32-nemu/src/lzip/quicklz.o: \
+ /home/curry/code/ysyx_curry/am-kernels/benchmarks/microbench/src/lzip/quicklz.c \
+ /home/curry/code/ysyx_curry/am-kernels/benchmarks/microbench/src/lzip/quicklz.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/am.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/arch/riscv.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/amdev.h \
+ /home/curry/code/ysyx_curry/abstract-machine/klib/include/klib.h
