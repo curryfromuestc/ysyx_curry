dfc4a2c1576cba6f747e446dff4b91b31b0296a2
tracer-ysyx <tracer@ysyx.org>
2025-04-03 15:50:30 +0800

    > compile NEMU ysyx_22040000 curryfromuestc Linux curry 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:30 up 4 days,  5:56,  1 user,  load average: 0.13, 0.05, 0.04

---
+++ am-kernels/kernels/demo/build/riscv32-nemu/src/ant/ant.d
@@ -0,0 +1,8 @@
+/home/curry/code/ysyx_curry/am-kernels/kernels/demo/build/riscv32-nemu/src/ant/ant.o: \
+ /home/curry/code/ysyx_curry/am-kernels/kernels/demo/src/ant/ant.c \
+ /home/curry/code/ysyx_curry/am-kernels/kernels/demo/include/io.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/am.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/arch/riscv.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/amdev.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/amdev.h \
+ /home/curry/code/ysyx_curry/abstract-machine/klib/include/klib-macros.h
---
+++ am-kernels/kernels/demo/build/riscv32-nemu/src/bf/bf.d
@@ -0,0 +1,10 @@
+/home/curry/code/ysyx_curry/am-kernels/kernels/demo/build/riscv32-nemu/src/bf/bf.o: \
+ /home/curry/code/ysyx_curry/am-kernels/kernels/demo/src/bf/bf.c \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/am.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/arch/riscv.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/amdev.h \
+ /home/curry/code/ysyx_curry/abstract-machine/klib/include/klib.h \
+ /home/curry/code/ysyx_curry/am-kernels/kernels/demo/include/io.h \
+ /home/curry/code/ysyx_curry/abstract-machine/am/include/amdev.h \
+ /home/curry/code/ysyx_curry/abstract-machine/klib/include/klib-macros.h \
+ /home/curry/code/ysyx_curry/am-kernels/kernels/demo/src/bf/mandelbrot.h
