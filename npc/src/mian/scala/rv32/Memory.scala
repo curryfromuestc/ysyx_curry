9c6aff72a320e0991051ad45d8140f6d66ff1577
curryfromuestc <curryfromuestc@gmail.com>
2025-04-17 18:30:09 +0800

    完成前三个stage的设计

--- npc/src/mian/scala/rv32/Memory.scala
+++ npc/src/mian/scala/rv32/Memory.scala
@@ -8,4 +8,8 @@ class ImemPortIo extends Bundle{
     val addr = Input(UInt(WORD_LEN.W))
     val inst = Output(UInt(WORD_LEN.W))
 }
-    
+class Memory(hexMemFile: String) extends Module{
+    val io = IO(new ImemPortIo)
+    val mem = Mem(1024,UInt(WORD_LEN.W))
+    io.inst := mem(io.addr)
+}
